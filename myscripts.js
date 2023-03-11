a8a17639d8d76e47aa12ed5261a264d99d932122
Tammy  Si <tammysi@Tammys-MacBook-Air.local>
2023-03-11 00:36:05 -0800

    The symbols now show up visually

--- myscripts.js
+++ myscripts.js
@@ -29,15 +29,18 @@ const displayController = (() => {
     const player2 = Player("O");
     let curr = player1;
 
-    const clickedBlock = (blockId, board) => {
+    const clickedBlock = (block, board) => {
         // clicked Location is an array with the block location the user clicked
-        let clickedLocation = blockId.split(" ")
+        let clickedLocation = block.id.split(" ")
+        console.log(clickedLocation)
         let x = Number(clickedLocation[0]);
         let y = Number(clickedLocation[1]);
         // make sure that the block the user clicked on is actually empty before we can put it in
         if (board.checkLocation(x, y)) {
             // if it's empty we can up the location in the board with the current player's symbol
             board.placeSymbol(x, y, curr.symbol);
+            // also change the DOM to match
+            block.textContent = curr.symbol;
             // this to alternate turn
             curr = curr == player1 ? player2 : player1;
         }
@@ -51,5 +54,5 @@ const displayController = (() => {
 // makes eventlisteners for block
 const allBlocks = document.querySelectorAll(".block")
 allBlocks.forEach((block) => block.addEventListener('click', () => {
-    displayController.clickedBlock(block.id, gameBoard);
+    displayController.clickedBlock(block, gameBoard);
 }))
