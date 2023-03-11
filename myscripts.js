243b718c8877001ce9ef2523832925c1a746bf0e
Tammy  Si <tammysi@Tammys-MacBook-Air.local>
2023-03-11 00:35:48 -0800

    Got board to update

--- myscripts.js
+++ myscripts.js
@@ -5,7 +5,17 @@ const gameBoard = (() => {
         ["", "", ""],
         ["", "", ""]
     ]
-    return {board}
+
+    const checkLocation = (x, y) => {
+        // if the location the user clicked on is empty, returns true
+        return board[x][y] == "";
+    }
+
+    const placeSymbol = (x, y, symbol) => {
+        board[x][y] = symbol;
+    }
+
+    return {board, checkLocation, placeSymbol}
 })();
 
 const Player = (sym) => {
@@ -19,15 +29,27 @@ const displayController = (() => {
     const player2 = Player("O");
     let curr = player1;
 
-    const clickedBlock = () => {
-        console.log(curr.symbol) 
-        curr = curr == player1 ? player2 : player1;
+    const clickedBlock = (blockId, board) => {
+        // clicked Location is an array with the block location the user clicked
+        let clickedLocation = blockId.split(" ")
+        let x = Number(clickedLocation[0]);
+        let y = Number(clickedLocation[1]);
+        // make sure that the block the user clicked on is actually empty before we can put it in
+        if (board.checkLocation(x, y)) {
+            // if it's empty we can up the location in the board with the current player's symbol
+            board.placeSymbol(x, y, curr.symbol);
+            // this to alternate turn
+            curr = curr == player1 ? player2 : player1;
+        }
+        console.log(curr)
     }
     console.log(player1);
     console.log(player2);
     return {clickedBlock}
 })();
 
-//
+// makes eventlisteners for block
 const allBlocks = document.querySelectorAll(".block")
-allBlocks.forEach((block) => block.addEventListener('click', displayController.clickedBlock))
+allBlocks.forEach((block) => block.addEventListener('click', () => {
+    displayController.clickedBlock(block.id, gameBoard);
+}))
